bcbcb1f249e70d8ce8552d1535b0ad1780050e16
Joseph Wangemann <fractalscape13@gmail.com>
2019-01-25 09:12:03 -0800

    create specs in README and create skeleton of if/else function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
   $(".enterNum").submit(function(event) {
     event.preventDefault();
+    var input = parseInt($("input#numInput").val());
     $(".well").hide();
     $(".output").show();
+    outputFunction(input);
   });
 });
+
+function outputFunction(input) {
+  if (input.includes("3")? true : false) {
+    $("#output3").show();
+  } else if (input.includes(2)? true : false) {
+    $("#output2").show();
+  } else if (input.includes(1)? true : false) {
+    $("#output1").show();
+  } else {
+    $("#outputNum").show();
+  }
+}
